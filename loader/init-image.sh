d584e5390887f02657db1b1da4ee89996b7117f8
Andrew W. Macpherson <mac@finnfet.co>
2024-01-23 13:28:14 +0200

    Simplified bee installation into

--- loader/init-image.sh
+++ loader/init-image.sh
@@ -11,4 +11,5 @@ BOOT_IMG=$1
 # main thread
 # fallocate -l 255m $BOOT_IMG # not supported in WSL
 dd if=/dev/zero of=$BOOT_IMG bs=1M count=512
-sgdisk --new=1:0:0 -t 1:ef00 $BOOT_IMG
+echo ',511M,U' | sfdisk $BOOT_IMG
+#sgdisk --new=1:0:0 -t 1:ef00 $BOOT_IMG
