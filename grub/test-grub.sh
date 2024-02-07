ec553db15b0a48d405b73f42009d43314b985f96
Chris Hobcroft <github@chrishobcroft.com>
2024-02-07 20:20:37 +0100

    Increased QEMU memory to 2048M
    Previous value of 1024M was causing Out-Of-Memory issues with the download of the bee appliance.

--- grub/test-grub.sh
+++ grub/test-grub.sh
@@ -18,6 +18,6 @@ mac=52:54:00:12:34:56
 ARCH=$(uname -m)
 dnf install -y qemu-system-${ARCH%%_64}
 
-qemu-system-$(uname -m) -smp 2 -m 1024M -nographic -enable-kvm -no-reboot \
+qemu-system-$(uname -m) -smp 2 -m 2048M -nographic -enable-kvm -no-reboot \
 	-bios "$BIOS" -drive file="$GRUB" \
 	-nic user,model=e1000,mac=$mac
